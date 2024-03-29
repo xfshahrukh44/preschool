858902bdfdc714c3c005d5ce7f500186e97fdc8f
xfshahrukh44 <ssidduit@gmail.com>
2024-03-29 23:26:42 +0500

    front changes

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -113,7 +113,7 @@
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="providerAlertLabel">Account required</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+{{--                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>--}}
                     </div>
                     <div class="modal-body">
                         <p>
@@ -125,9 +125,9 @@
                         <hr>
                         <span>Already have a provider account? <a href="{{route('signin')}}" class="text-center">Login</a>.</span>
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-primary">Understood</button>
-                    </div>
+{{--                    <div class="modal-footer">--}}
+{{--                        <button type="button" class="btn btn-primary">Understood</button>--}}
+{{--                    </div>--}}
                 </div>
             </div>
         </div>
--- resources/views/provider_menues.blade.php
+++ resources/views/provider_menues.blade.php
@@ -25,11 +25,11 @@ $segment2 = Request::segment(2);
                 @endif
 
                 {{--Find daycare--}}
-                <li><a class="<?php if($segment2 == "daycare"){ echo 'menu_active'; } ?>" href="{{route('provider.findDaycare')}}"><img width="20" src="{{asset('images/search.png')}}"> Daycare </a></li>
+                <li><a class="<?php if($segment2 == "daycare"){ echo 'menu_active'; } ?>" href="{{route('provider.findDaycare')}}"><img width="20" src="{{asset('images/search.png')}}"> Center </a></li>
 
                 {{--Claimed Centers--}}
                 <li><a class="<?php if($segment2 == "claimed-centers"){ echo 'menu_active'; } ?>" href="{{route('provider.claimedCenters')}}"><img width="20" src="{{asset('images/search.png')}}"> Claimed Centers </a></li>
-
+da
                 <li><a class="<?php if($segment == "update-provider-profile"){ echo 'menu_active'; } ?>" href="{{route('update_provider_profile')}}"><img src="{{asset('images/group511.png')}}">Profile Update</a></li>
                 <li><a class="<?php if($segment == ""){ echo 'menu_active'; } ?>" href="{{ URL('signout') }}"><img src="{{asset('images/logout1.png')}}">Logout</a></li>
             </ul>

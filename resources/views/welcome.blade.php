5308b0d68efb330b118ad3078674a98965597518
xfshahrukh44 <ssidduit@gmail.com>
2024-05-31 03:35:19 +0500

    changes 31/05.2024

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -248,7 +248,7 @@
     <!--</section>-->
 
 
-    <section class="sec-two">
+    <section class="sec-two" style="background: url({{asset('images/doodle-wall.png')}})">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-12">

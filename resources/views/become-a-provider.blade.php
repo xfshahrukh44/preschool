a959f0bf974077d14d803d8564190647b8426610
Irtiza Ahmed <54776755+IrtizaAhmed131198@users.noreply.github.com>
2025-03-22 00:53:00 +0500

    taoster

--- resources/views/become-a-provider.blade.php
+++ resources/views/become-a-provider.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('css')
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
     <style>
         .about-sec-one {
             /*background-image: url(<?php //echo asset('images/194-scaled.jpg');
@@ -446,9 +446,7 @@
 
 @section('js')
     <script src="https://www.paypalobjects.com/api/checkout.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.js"
-        integrity="sha512-zlWWyZq71UMApAjih4WkaRpikgY9Bz1oXIW5G0fED4vk14JjGlQ1UmkGM392jEULP8jbNMiwLWdM8Z87Hu88Fw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 
 
     <script>
